0ad1410fc017dd02b09e594ac4e41ed97e22ca03
Gökay Baz <gokaybaz2000@outlook.com>
2025-01-31 16:02:46 +0300

    [Changed] NavButton Title Text.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
             <a href="#" class="text-2xl font-bold text-indigo-600">Baz Todo</a>
             <div class="space-x-4">
                 <a href="#" class="text-gray-700 hover:text-indigo-500 font-medium">Anasayfa</a>
-                <a href="#" class="text-gray-700 hover:text-indigo-500 font-medium">Görevlerim</a>
+                <a href="#" class="text-gray-700 hover:text-indigo-500 font-medium">Planlanmış Görevlerim</a>
             </div>
             <div class="flex items-center space-x-4">
                 <img
